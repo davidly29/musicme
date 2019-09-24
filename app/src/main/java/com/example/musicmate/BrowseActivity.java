920003af8771bbcafbb03072666dbca60cc0bf98
davidly29 <davidly255@gmail.com>
2019-09-24 17:36:50 +0100

    passing params

--- app/src/main/java/com/example/musicmate/BrowseActivity.java
+++ app/src/main/java/com/example/musicmate/BrowseActivity.java
@@ -3,12 +3,21 @@ package com.example.musicmate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class BrowseActivity extends AppCompatActivity {
+    TextView tv;
+
+    String nameValue ="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse);
+        tv.findViewById(R.id.textView);
+
+        nameValue = getIntent().getExtras().getString("Value");
+        tv.setText(nameValue);
+
     }
 }
--- app/src/main/java/com/example/musicmate/MainActivity.java
+++ app/src/main/java/com/example/musicmate/MainActivity.java
@@ -6,9 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    private Button goToSecond;
+    Button goToSecond;
+    String name;
+
+    EditText username;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +21,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         goToSecond = (Button) findViewById(R.id.button);
-        goToSecond.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, BrowseActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
+        username = (EditText) findViewById(R.id.username);
 
+
+        goToSecond.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, BrowseActivity.class);
+            name = username.getText().toString();
+            intent.putExtra("Name", name);
+            startActivity(intent);
+         }
+        }
+
+
+        );
+    }
 
 }
